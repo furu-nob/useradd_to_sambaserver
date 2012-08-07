61f6d32b5dd3d913bfdacd7e8d631f8899cb74f7
Nobuyuki Furuyama <furu_nob@yahoo.co.jp>
2012-08-07 18:38:02 +0900

    Add err

--- useradd.sh
+++ useradd.sh
@@ -9,14 +9,19 @@ function usage(){
     exit 0
 }
 
-if [ $# -ne 2 ]
+if [ $# -eq 2 ]
 then
     useradd -d /dev/null -g smbusers -s /sbin/nologin $USERID
-    echo -e "$PASSWORD\n$PASSWORD\n" | pdbedit -t -a -u $USERID
-    echo "Success!"
-    echo "UserID = $USERID"
-    echo "Password = $PASSWORD"
-    
+    if [ $? -eq 0 ]
+    then
+        echo -e "$PASSWORD\n$PASSWORD\n" | pdbedit -t -a -u $USERID
+        echo "Success!"
+        echo "UserID = $USERID"
+        echo "Password = $PASSWORD"
+    else
+            echo "useradd Err !"
+            exit 1
+    fi
 else
     usage
 fi
